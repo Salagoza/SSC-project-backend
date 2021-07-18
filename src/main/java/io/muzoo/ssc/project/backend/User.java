62a3ef8d4e89a6dd13966a380527c7e01dbb0550
Thanawat <lordcopterXI@gmail.com>
2021-07-18 13:17:52 +0700

    More things added !!
    Can't connect with mariadb so use mysql instead added. Added spring data , jpa, User entity and UserRepo.

---
+++ src/main/java/io/muzoo/ssc/project/backend/User.java
@@ -0,0 +1,37 @@
+package io.muzoo.ssc.project.backend;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+
+    @Column(name = "ID")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String userName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "DOB")
+    private Date dateOfbirth;
+
+    @Column(name = "password")
+    private String password;
+
+}
